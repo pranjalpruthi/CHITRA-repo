5a5d5f191352bbd908e2ef987d4eb47d2ee8034a
Pranjal Pruthi <47497714+pranjalpruthi@users.noreply.github.com>
2025-01-09 12:31:13 +0530

    bug fix on doc added holocron as img host ⚡️
    should work in production now

--- next.config.mjs
+++ next.config.mjs
@@ -59,6 +59,11 @@ const nextConfig = {
       hostname: 'media.licdn.com',
       port: '',
       pathname: '/**'
+    }, {
+      protocol: 'https',
+      hostname: 'holocron.so',
+      port: '',
+      pathname: '/**'
     }]
   }
 };
